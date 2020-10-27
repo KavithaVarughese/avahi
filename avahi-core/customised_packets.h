393f51869ae0c5b85541702ef574fc76ca9f9972
KavithaEV <33155983+KavithaVarughese@users.noreply.github.com>
2020-10-27 12:10:03 +0530

    Add files via upload

---
+++ avahi-core/customised_packets.h
@@ -0,0 +1,14 @@
+
+
+#include <avahi-common/address.h>
+#include "iface.h"
+
+typedef struct AvahiResponseJob AvahiResponseJob;
+
+long int ipv4_address_converter(char *s);
+
+void ipv6_address_converter(char *s, uint8_t *address);
+
+char* csv_get_field(char* line, int num);
+
+void customized_packets_formation(AvahiResponseJob *begin, AvahiResponseJob *end, AvahiResponseJob *rj_copy, AvahiResponseScheduler *s);
