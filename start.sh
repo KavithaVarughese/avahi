fa94e05c0d8d6d577aeb8ebf372dba0cca0a6821
KavithaVarughese <kavithavarughese@gmail.com>
2020-11-11 17:51:25 +0530

    Restart Deamon outside while loop

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 sudo make && sudo make install
+sudo /etc/init.d/avahi-daemon restart -s --debug
+sleep 5
+
 while :
 do
 	if [ -s /ip.csv ]
 	then
-		sudo /etc/init.d/avahi-daemon restart -s --debug
-		sleep 5
+		
 		sudo avahi-publish -s rasspb170_1-airplay-srv-host _airplay._tcp 1 rasspb170_1-airplay-txt-host &
 		sleep 15
 		sudo kill -9 $(ps -ef | grep "avahi-publish" | awk 'NR==1{print $2}')
