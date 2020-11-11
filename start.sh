486f97a4e65a5679b43a9066a9ce6225ebce92ed
KavithaVarughese <kavithavarughese@gmail.com>
2020-11-12 01:08:43 +0530

    Restart daemon outside while loop in start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 sudo make && sudo make install
+sudo /etc/init.d/avahi-daemon restart -s --debug
+sleep 5
 while :
 do
 	if [ -s /browse_service.csv ]
 	then
-		sudo /etc/init.d/avahi-daemon restart -s --debug
-		sleep 5
+		
 		sudo avahi-browse _chromium._tcp &
-		sleep 15
+		sleep 10s
 		sudo kill -9 $(ps -ef | grep "avahi-browse" | awk 'NR==1{print $2}')
+		sleep 5s
 	else
 		sleep 20
 	fi
